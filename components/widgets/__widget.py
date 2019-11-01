f5488c1fb0bf9142baae5dda8be9714268f61627
square1grp <developer01@square1grp.com>
2019-11-01 03:49:20 -0700

    responsive for xl-bar

--- components/widgets/__widget.py
+++ components/widgets/__widget.py
@@ -15,57 +15,34 @@ class __Widget:
         self.config = config
         self.widget_type = widget_type
         self.is_child_widget = is_child_widget
-    
-    # get keys of graph_obj
-    def get_attr_key_list(self, attr_type):
-        if 'bar' in self.widget_type:
-            if attr_type == 'layout':
-                return ['title', 'xaxis', 'yaxis', 'showlegend']
-            elif attr_type == 'marker':
-                return ['colorscale', 'line_color', 'line_width', 'showscale', 'colorbar']
-    
-        return []
-    
-    # get marker
-    def get_marker(self, data):
-        go_marker = dict()
-
-        if 'marker' not in data:
-            return go_marker
-        
-        if 'colorscale' in data['marker']:
-            go_marker['color'] = data['y']
-        
-        for key in self.get_attr_key_list('marker'):
-            if key in data['marker']:
-                go_marker[key] = data['marker'][key]
-        
-        return go_marker
-        
-    # get title area
-    def get_title(self, title):
-        return html.Div(
-            html.H4(title['text'] if 'text' in title else ''),
-            className='title %s' % ('text-%s' % title['transform']) if 'transform' in title else None
-        )
-
-    # return graph data
-    def get_data(self):
-        return None
 
     # return graph layout
-    def get_layout(self):
+    def get_graph_layout(self, options=dict()):
         layout = self.config['graph']['layout']
 
         go_layout = dict()
 
-        for key in self.get_attr_key_list('layout'):
+        for key in ['title', 'xaxis', 'yaxis', 'showlegend']:
             if key in layout:
                 go_layout[key] = layout[key]
 
         go_layout['paper_bgcolor'] = 'rgba(0,0,0,0)'
         go_layout['plot_bgcolor'] = 'rgba(0,0,0,0)'
+
+        go_layout.update(options)
+
         return go_layout
+        
+    # get title area
+    def get_widget_title(self, title):
+        return html.Div(
+            html.H4(title['text'] if 'text' in title else ''),
+            className='title %s' % ('text-%s' % title['transform']) if 'transform' in title else None
+        )
+
+    # get graph in the current widget
+    def get_widget_graph(self):
+        return []
 
     # content which draws a widget
     # default: colored rect range 
@@ -73,17 +50,10 @@ class __Widget:
         widget_content = []
 
         if 'title' in self.config:
-            widget_content.append(self.get_title(self.config['title']))
+            widget_content.append(self.get_widget_title(self.config['title']))
         
-        widget_content.append(
-            dcc.Graph(
-                figure=go.Figure(
-                    data=self.get_data(),
-                    layout=self.get_layout()
-                ),
-                className='m-auto'
-            )
-        )
+        for graph in self.get_widget_graph():
+            widget_content.append(graph)
 
         return html.Div(
             widget_content,
--- components/widgets/xl-bar.py
+++ components/widgets/xl-bar.py
@@ -1,11 +1,27 @@
 from .__widget import __Widget
 import plotly.graph_objs as go
+import dash_core_components as dcc
 
 
-class Widget(__Widget):
+class Widget(__Widget):    
+    # get data marker
+    def get_data_marker(self, data, marker_props):
+        go_marker = dict()
+
+        if 'marker' not in data:
+            return go_marker
+        
+        for prop in marker_props:
+            if prop in data['marker']:
+                go_marker[prop] = data['marker'][prop]
+        
+                if prop == 'colorscale':
+                    go_marker['color'] = data['y']
+        
+        return go_marker
 
     # generate data for bar chart
-    def get_data(self):
+    def get_graph_data(self, marker_props=[]):
         data_arr = self.config['graph']['data']
 
         return [
@@ -13,6 +29,33 @@ class Widget(__Widget):
                 x=data['x'],
                 y=data['y'],
                 name=data['name'] if 'name' in data else None,
-                marker=self.get_marker(data)
+                marker=self.get_data_marker(data, marker_props)
             ) for data in data_arr
+        ]
+    
+    # get mobile layout
+    def get_mobile_layout(self):
+        return dict(
+            margin=dict(b=20, t=40, r=20)
+        )
+    
+    # get graph in the current widget
+    def get_widget_graph(self):
+        mobile_layout = self.get_mobile_layout()
+
+        return [
+            dcc.Graph(
+                figure=go.Figure(
+                    data=self.get_graph_data(marker_props=['colorscale', 'line_color', 'line_width', 'showscale', 'colorbar']),
+                    layout=self.get_graph_layout()
+                ),
+                className='m-auto d-none d-xl-block'
+            ),
+            dcc.Graph(
+                figure=go.Figure(
+                    data=self.get_graph_data(marker_props=['colorscale', 'line_color', 'line_width']),
+                    layout=self.get_graph_layout(mobile_layout)
+                ),
+                className='m-auto d-xl-none'
+            )
         ]
