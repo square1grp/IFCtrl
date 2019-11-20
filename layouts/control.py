f08f6099ff70aa176707bbef69a55cf5d3974f53
square1grp <developer01@square1grp.com>
2019-11-20 01:55:38 -0800

    update

--- layouts/control.py
+++ layouts/control.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from classes.User import User
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 cur_user = User.get_instance()
@@ -26,7 +26,7 @@ def get_layout():
                                            className='m-0'),
                                 dcc.DatePickerSingle(
                                     id='time_stamp_from',
-                                    date=datetime.today().strftime('%Y-%m-%d'),
+                                    date=(datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d'),
                                     max_date_allowed=datetime.today(),
                                     display_format='MM / DD'
                                 )
