927a6b030491b43661cd668152a31d5cb331e6f1
M Asad <asad@reporteq.com>
2019-11-21 01:33:13 +0500

    Replace panda item method to avoid future warnings, applied check to convert parameters into timestamp object if they were supplied as string, retured empty array instead none if no data found added data_tag flag for data source confirmation in debugging

--- querymanager/intelliflux_querymanager.py
+++ querymanager/intelliflux_querymanager.py
@@ -22,10 +22,17 @@ class IntelliFluxQueryManager(object):
 
     """
     def getWidgetDataFromQueryManager(self,widget_name,time_stamp_from, time_stamp_to,database_id,sql_query,user_id,is_json_reponse):
+        master_data_frame = None
+        widget_data = None
         try:
+            data_tag = 'Empty'
+            #If supplied arrgument for required dataset time period are of type string then convert it into timestamp 
+            if type(time_stamp_from) is str:
+                time_stamp_from = datetime.strptime(time_stamp_from, '%Y-%m-%d')
+                time_stamp_to = datetime.strptime(time_stamp_to, '%Y-%m-%d')
             cache_expiry_date = datetime.now() + timedelta(days=-30)
             widget_name = str(user_id)+'_'+str(database_id)+'_'+widget_name.replace(" ", "_")
-            widget_data = None
+            
             master_data_frame = self.loadMasterDataFrame()
             #print(master_data_frame)
             #return master_data_frame
@@ -38,22 +45,32 @@ class IntelliFluxQueryManager(object):
                     if time_stamp_from is not None and time_stamp_to is not None: 
                         widget_data.to_pickle('panda_cache/'+widget_name+'.pkl')
                         self.addNewRefrenceInMasterDataframe(master_data_frame,widget_name,user_id,database_id,time_stamp_from,time_stamp_to)
+                        data_tag = 'From API - No Cached Data'
             else:
                 cache_data_detail_df = self.getWidgetDataFrameRefrence(master_data_frame,widget_name,user_id,database_id)
                 if cache_data_detail_df.count().widget_name == 1:
                     cached_widget_data = self.loadCachedDataFrames(widget_name)
                     panda_Sql_query = self.getPandaSqlQuery(sql_query,'cached_widget_data')
-                    if time_stamp_from is not None and time_stamp_to is not None and cache_data_detail_df.last_modification_time.item() > cache_expiry_date:
-                        if time_stamp_from >= cache_data_detail_df.time_stamp_from.item()  and time_stamp_to <= cache_data_detail_df.time_stamp_to.item():
+
+                    #Get cached data last modification date to validate cache expiry
+                    cached_data_last_modification_date = next(iter(cache_data_detail_df.last_modification_time), 'no match')
+                    #Get Starting date of cached data from master cache
+                    cached_data_start_date = next(iter(cache_data_detail_df.time_stamp_from), 'no match')
+                    #Get end date of cached data from master cache
+                    cached_data_end_date = next(iter(cache_data_detail_df.time_stamp_to), 'no match')
+
+                    if time_stamp_from is not None and time_stamp_to is not None and cached_data_last_modification_date > cache_expiry_date:
+                        if time_stamp_from >= cached_data_start_date  and time_stamp_to <= cached_data_end_date:
                             widget_data = psql.sqldf(panda_Sql_query, locals())
-                            print(widget_data)
-                        elif  time_stamp_from > cache_data_detail_df.time_stamp_to.item() or time_stamp_to > cache_data_detail_df.time_stamp_to.item():
-                            modified_query = replace_date_filters(sql_query,time_stamp_from,cache_data_detail_df.time_stamp_to.item())
+                            data_tag = 'From Cache'
+                        elif  time_stamp_from > cached_data_end_date or time_stamp_to > cached_data_end_date:
+                            modified_query = replace_date_filters(sql_query,time_stamp_from,cached_data_end_date)
                             json_widget_data = getWidgetData(database_id,modified_query)
                             widget_data_from_db =  pd.read_json(json_widget_data)
                             cached_widget_data = cached_widget_data.append(widget_data_from_db, ignore_index = True) 
                             widget_data =  psql.sqldf(panda_Sql_query, locals())
-                            self.updateMasterDataFrame(master_data_frame,widget_name,user_id,database_id,cache_data_detail_df.time_stamp_from.item(),time_stamp_to)
+                            self.updateMasterDataFrame(master_data_frame,widget_name,user_id,database_id,cached_data_start_date,time_stamp_to)
+                            data_tag = 'From Cache & API'
                         else:
                             print("No matched")
                     else:
@@ -61,15 +78,19 @@ class IntelliFluxQueryManager(object):
                          widget_data =  pd.read_json(json_widget_data)
                          widget_data.to_pickle('panda_cache/'+widget_name+'.pkl')
                          self.updateMasterDataFrame(master_data_frame,widget_name,user_id,database_id,time_stamp_from,time_stamp_to)
+                         data_tag = 'From API due to Cache Expiry'
                 else:
                     json_widget_data = getWidgetData(database_id,sql_query)
                     widget_data =  pd.read_json(json_widget_data)
                     widget_data.to_pickle('panda_cache/'+widget_name+'.pkl')
                     self.addNewRefrenceInMasterDataframe(master_data_frame,widget_name,user_id,database_id,time_stamp_from,time_stamp_to)
+                    data_tag = 'From API - No Cached Data'
         except (Exception) as error:
             print(error)
         finally:
-            print('Process completed.')
+            print('Process completed with data_tag: '+data_tag)
+            if widget_data is None:
+                widget_data = {}
 
         return  widget_data
 
@@ -77,10 +98,10 @@ class IntelliFluxQueryManager(object):
     def loadMasterDataFrame(self):
         try:
             df = pd.read_pickle("panda_cache/master.pkl")
-            print(df)
+            #print(df)
             return df
         except (Exception) as error:
-            print(error)
+            #print(error)
             return None
 
     
@@ -88,7 +109,7 @@ class IntelliFluxQueryManager(object):
     def loadCachedDataFrames(self,widget_name):
         try:
             df = pd.read_pickle('panda_cache/'+widget_name+'.pkl')
-            print('Cached data retured for '+widget_name)
+            #print('Cached data retured for '+widget_name)
             return df
         except (Exception) as error:
             print(error)
@@ -113,18 +134,19 @@ class IntelliFluxQueryManager(object):
             updated_df.to_pickle('panda_cache/master.pkl')
             status = True
         except (Exception) as error:
+            print('addNewRefrenceInMasterDataframe:')
             print(error)
         return status
 
     #Update Master Dataframe incase of change in date filters
-    def updateMasterDataFrame(self,dataFrameToUpdate,widget_name,user_id,database_id,time_stamp_from,time_stamp_to):
+    def updateMasterDataFrame(self,master_data_frame,widget_name,user_id,database_id,time_stamp_from,time_stamp_to):
         status = False
         try:
             current_date_time = datetime.now()
-            dataFrameToUpdate.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'time_stamp_from'] = time_stamp_from
-            dataFrameToUpdate.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'time_stamp_to'] = time_stamp_to
-            dataFrameToUpdate.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'last_modification_time'] = current_date_time
-            dataFrameToUpdate.to_pickle('panda_cache/master.pkl')
+            master_data_frame.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'time_stamp_from'] = time_stamp_from
+            master_data_frame.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'time_stamp_to'] = time_stamp_to
+            master_data_frame.loc[(master_data_frame['widget_name'] == widget_name) & (master_data_frame['user_id'] == user_id) & (master_data_frame['database_id'] == database_id), 'last_modification_time'] = current_date_time
+            master_data_frame.to_pickle('panda_cache/master.pkl')
             status = True
         except (Exception) as error:
             print(error)
